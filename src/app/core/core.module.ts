5d75a56b43c21c8cb56725e48abc62fb1f1d4642
Michael Ian Verastigue <michaelianverastigue@gmail.com>
2021-02-27 13:36:16 +0800

    added footer and header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './header/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RecipeComponent,
+    HomeComponent,
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
